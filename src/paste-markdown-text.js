01f8047a3e5be6d76d027285d94b117cc3bed939
David Graham <david.malcom.graham@gmail.com>
2019-02-25 16:45:04 -0700

    Paste markdown text from clipboard
    Pastes markdown formatted text from the clipboard with the custom
    text/x-gfm mime type if available. Pairs with`@github/quote-selection`
    which copies the text/x-gfm data onto the clipboard.

---
+++ src/paste-markdown-text.js
@@ -0,0 +1,31 @@
+/* @flow strict */
+
+import {insertText} from './text'
+
+export function install(el: Element) {
+  el.addEventListener('paste', onPaste)
+}
+
+export function uninstall(el: Element) {
+  el.removeEventListener('paste', onPaste)
+}
+
+function onPaste(event: ClipboardEvent) {
+  const transfer = event.clipboardData
+  if (!transfer || !hasMarkdown(transfer)) return
+
+  const field = event.currentTarget
+  if (!(field instanceof HTMLTextAreaElement)) return
+
+  const text = transfer.getData('text/x-gfm')
+  if (!text) return
+
+  event.stopPropagation()
+  event.preventDefault()
+
+  insertText(field, text)
+}
+
+function hasMarkdown(transfer: DataTransfer): boolean {
+  return Array.from(transfer.types).indexOf('text/x-gfm') >= 0
+}
