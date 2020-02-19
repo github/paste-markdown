613f66260d3e96d800f98bc82d84aeb903b35ca0
Mu-An Chiou <me@muanchiou.com>
2020-02-19 13:43:36 -0500

    Add support back to Input elements Note: selectionStart/End only applies to input elements of certain types so it could be null and throws. This fallbacks to 0. https://html.spec.whatwg.org/#do-not-apply

--- src/text.ts
+++ src/text.ts
@@ -1,6 +1,6 @@
-export function insertText(textarea: HTMLTextAreaElement, text: string): void {
-  const beginning = textarea.value.substring(0, textarea.selectionStart)
-  const remaining = textarea.value.substring(textarea.selectionEnd)
+export function insertText(textarea: HTMLInputElement | HTMLTextAreaElement, text: string): void {
+  const beginning = textarea.value.substring(0, textarea.selectionStart || 0)
+  const remaining = textarea.value.substring(textarea.selectionEnd || 0)
 
   const newline = beginning.length === 0 || beginning.match(/\n$/) ? '' : '\n'
   const textBeforeCursor = beginning + newline + text
